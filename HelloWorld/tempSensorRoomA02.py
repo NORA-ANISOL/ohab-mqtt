26ea82d0dcdde267303d8c78ed8fca276966d41a
NORA ANISOL VELAZQUEZ MORALES <NORA ANISOL VELAZQUEZ MORALES>
2017-05-06 22:03:15 -0500

    hola mundo mqtt

---
+++ HelloWorld/tempSensorRoomA02.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import time
+
+Celsius = 0
+mqttc = mqtt.Client("tempSensorRoomA02")
+#mqttc.username_pw_set(openhabian, password=rest1sum3)
+mqttc.connect("192.168.1.136", 1883)
+while True:
+	mqttc.publish("myHome/tempSensor/RoomA/02", Celsius)
+	#mqttc.loop(2) #timeout = 2s
+	Celsius = Celsius + 1
+	if Celsius >= 100:
+		Celsius = 0
+	time.sleep(4)
+
+#Number homeTemp "Temperature [%.1f °C]" {mqtt="<[MulCam:myHome/temperature:state:default]"}
